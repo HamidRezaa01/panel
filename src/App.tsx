5c0833ce32bf9c2cb715a4c2d7d3b501db3ad44f
HamidReza <mazinanihamidreza80@gmail.com>
2024-07-02 22:12:12 +0330

    updload test project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import NavBar from '../src/Components/NavBar/NavBar'
+import Sidebar from './Components/SideBar/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './Components/pages/Dashboard'
+
+const App = () => {
+  const [sidebarToggle, setSidebarToggle] = useState(false)
+
+  return (
+    <>
+      <Sidebar sidebarToggle={sidebarToggle}/>
+      <NavBar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle}/>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SideBar/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { MdSpaceDashboard } from "react-icons/md";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineRoundaboutRight } from "react-icons/md";
+
+type SidebarToggle = {
+    sidebarToggle: boolean
+}
+
+const Sidebar = ({sidebarToggle}: SidebarToggle) => {
+    
+    return(
+        <div className={`${sidebarToggle ? "hidden" : "block"} w-64 bg-slate-700 fixed h-full border-r-2 p-4`}>
+            <nav className="mt-3 text-white font-bold list-none">
+                <li className="mb-2 rounded hover:shadow hover:bg-slat-800 py-2 cursor-pointer">
+                    <a href="#" className="px-3">
+                        <MdSpaceDashboard  className="inline-block w-6 h-6 mr-2 -mt-2"></MdSpaceDashboard >
+                        Dashboard
+                    </a>
+                </li>
+                <li className="mb-2 rounded hover:shadow hover:bg-slat-800 py-2 cursor-pointer">
+                    <a href="#" className="px-3">
+                        <MdOutlineProductionQuantityLimits  className="inline-block w-6 h-6 mr-2 -mt-2"></MdOutlineProductionQuantityLimits >
+                        Products
+                    </a>
+                </li>
+                <li className="mb-2 rounded hover:shadow hover:bg-slat-800 py-2 cursor-pointer">
+                    <a href="#" className="px-3">
+                        <CgProfile  className="inline-block w-6 h-6 mr-2 -mt-2"></CgProfile >
+                        Profile
+                    </a>
+                </li>
+                <li className="mb-2 rounded hover:shadow hover:bg-slat-800 py-2 cursor-pointer">
+                    <a href="#" className="px-3">
+                        <MdOutlineRoundaboutRight className="inline-block w-6 h-6 mr-2 -mt-2"></MdOutlineRoundaboutRight>
+                        About
+                    </a>
+                </li>
+            </nav>
+        </div>
+    )
+}
+
+export default Sidebar
