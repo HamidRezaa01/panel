25fe15414f30e985ded8f9e29bfbdf2b2851bf1d
HamidReza <mazinanihamidreza80@gmail.com>
2024-07-06 12:41:58 +0330

    import user dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import NavBar from '../src/Components/NavBar/NavBar'
-import Sidebar from './Components/SideBar/Sidebar'
 import { Routes, Route } from 'react-router-dom'
-import Dashboard from './Components/pages/Dashboard'
+import Dashboard from './UserDashboard/Components/Pages/Dashboard'
+import UserDashboardLayout from './UserDashboard/Components/UserDashboardLayout'
 
 const App = () => {
-  const [sidebarToggle, setSidebarToggle] = useState(false)
 
   return (
     <>
-      <Sidebar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle}/>
-      <NavBar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle}/>
+      <UserDashboardLayout/>
       <Routes>
         <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
